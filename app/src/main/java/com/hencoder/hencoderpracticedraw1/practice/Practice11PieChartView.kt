e6434b217c145e31a6eb91b3fa698d025695bd8d
Walker Zhao <zhaocework@gmail.com>
2017-09-22 11:36:04 +0800

    修改自定义View的多个构造函数声明方式

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.kt
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.kt
@@ -5,13 +5,11 @@ import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 
-class Practice11PieChartView : View {
-
-    constructor(context: Context) : super(context) {}
-
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {}
-
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
+class Practice11PieChartView @JvmOverloads constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
